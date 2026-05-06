7df09b2fdcc4e4da1ec8ca2daf5f312083a584d2
GimmeSpork <lucy.price@pcc.edu>
2026-05-06 03:46:40 +0000

    commit

--- main.cpp
+++ main.cpp
@@ -1,80 +1,120 @@
-// ------------- FILE HEADER -------------
-// Author ✅: 
-// Assignment ✅:
-// Date ✅:
-// Citations: 
-
-
-// ------------- ZYBOOKS SCORES -------------
-// Chapter ✅: 
-// Participation ✅: 
-// Challenge ✅:
-// Labs ✅:
-
-
-// ------------- DISCORD POSTS -------------
-// https://discord.com/invite/URYKKf8YHm
-// Count ✅:
-// Links (Optional): 
-
-
-// ------------- DESIGN DOCUMENT -------------
-// A. INPUT ✅: 
-// B. OUTPUT ✅:
-// C. CALCULATIONS ✅:
-// D. LOGIC and ALGORITHMS ✅:
-//    (Optional) flow chart link or file name: 
-
-
-// ------------- TESTING -------------
-// PASS ALL GIVEN SAMPLE RUN TESTS ✅: 
-// (Optional) Additional tests count:   
-
-
-// ------------- CODE -------------
+/******************************************************************************
+# Author:           Lucy Price
+# Assignment:       Assignment 4 - Phrases and Subphrases program
+# Date:             April 30, 2026
+# Description:      This program will take string inputs from the user and
+#                   output whether or not phrase 1 was found in phrase 2 or
+#                   the other way around. It will also say if the phrases
+#                   match, or do not match. It will then print the phrase
+#                   that matches, and where it starts in the other phrase.
+# Input:            phrase1 and phrase2 as strings.
+# Output:           phrase1 and phrase2 as strings.
+# Sources:          Assignement 4 resources, recorded zoom of class and
+#                   zybooks, chapter 4 section 4.12.
+#******************************************************************************/
 #include <iostream>
-
+#include <string>
+#include <iomanip>
+#include <limits>
 using namespace std;
 
-// Function prototypes (if any)
-
-
 // Main function
-// https://en.cppreference.com/w/cpp/language/main_function.html
-int main(int argc, char* argv[]) {
-  cout << "Hello, World!" << endl;
+int main() {
+
+  //declare variables as strings and ints
+  int const NUM_PLAYERS = 3;
+  string player1;
+  string player2;
+  string player3;
+  int rocks1 = 0;
+  int rocks2 = 0;
+  int rocks3 = 0;
+
+  //welcome message
+  cout << "Welcome to the Rock Collector Championships!" << endl;
+  //player 1
+  cout << "\nEnter player 1 name: ";
+  getline(cin, player1);
+  cout << "How many rocks did " << player1 << " collect?: ";
+  cin >> rocks1;
+  cin.ignore(numeric_limits<streamsize>::max(),'\n');
+  if (rocks1 < 0) {
+    cout << "Invalid amount, 0 will be entered." << endl;
+    rocks1 = 0;
+  }
+  //player 2
+  cout << "\nEnter player 2 name: ";
+  getline(cin, player2);
+  cout << "How many rocks did " << player2 << " collect?: ";
+  cin >> rocks2;
+  cin.ignore(numeric_limits<streamsize>::max(),'\n');
+  if (rocks2 < 0) {
+    cout << "Invalid amount, 0 will be entered." << endl;
+    rocks2 = 0;
+  }
+
+  //player 3
+  cout << "\nEnter player 3 name: ";
+  getline(cin, player3);
+  cout << "How many rocks did " << player3 << " collect?: ";
+  cin >> rocks3;
+  cin.ignore(numeric_limits<streamsize>::max(),'\n');
+  if (rocks3 < 0) {
+    cout << "Invalid amount, 0 will be entered." << endl;
+    rocks3 = 0;
+  }
+
+  //calculations and winners
+  //first place
+  if (rocks1 > rocks2 && rocks1 > rocks3) {
+    cout << "\n" << player1 << " is in first place!" << endl;
+  }else if (rocks2 > rocks1 && rocks2 > rocks3) {
+    cout << "\n" << player2 << " is in first place!" << endl;
+  }else if (rocks3 > rocks1 && rocks3 > rocks2) {
+    cout << "\n" << player3 << " is in first place!" << endl;
+  }else if (rocks1 == rocks2 && rocks1 > rocks3) {
+    cout << "\n" << player1 << " and " << player2 << " are tied for first!." << endl;
+  }else if (rocks1 == rocks3 && rocks1 > rocks2) {
+    cout << "\n" << player1 << " and " << player3 << " are tied for first!." << endl;
+  }else if (rocks2 == rocks3 && rocks2 > rocks1) {
+    cout << "\n" << player2 << " and " << player3 << " are tied for first!." << endl;
+  }else if (rocks1 == rocks2 && rocks1 == rocks3) {
+    cout << "\nIt's a three way tie!" << endl;
+  }
+
+  //second place
+  if ((rocks1 > rocks2 && rocks1 < rocks3) || (rocks1 > rocks3 && rocks1 < rocks2)) {
+    cout << player1 << " is in second place!" << endl;
+  }else if ((rocks2 > rocks1 && rocks2 < rocks3) || (rocks2 > rocks3 && rocks2 < rocks1)) {
+    cout << player2 << " is in second place!" << endl;
+  }else if ((rocks3 > rocks1 && rocks3 < rocks2) || (rocks3 < rocks1 && rocks3 > rocks2)) {
+    cout << player3 << " is in second place!" << endl;
+  }else if (rocks1 == rocks2 && rocks1 < rocks3) {
+    cout << player1 << " and " << player2 << " are tied for second place!" << endl;
+  }else if (rocks1 == rocks3 && rocks1 < rocks2) {
+    cout << player1 << " and " << player3 << " are tied for second place!" << endl;
+  }else if (rocks2 == rocks3 && rocks2 < rocks1) {
+    cout << player2 << " and " << player3 << " are tied for second place!" << endl;
+  }
+
+  //third place
+  if (rocks1 < rocks2 && rocks1 < rocks3) {
+    cout << player1 << " is in third place!" << endl;
+  }else if (rocks2 < rocks1 && rocks2 < rocks3) {
+    cout << player2 << " is in third place!" << endl;
+  }else if (rocks3 < rocks1 && rocks3 < rocks2) {
+    cout << player3 << " is in third place!" << endl;
+  }
+  
+  //calculations for rock average as double
+  cout << fixed << setprecision(2);
+  double avgRocks = (rocks1 + rocks2 + rocks3) / NUM_PLAYERS;
+  cout << "\nThe average number of rocks collected by the top three players " 
+  "is " << avgRocks << " rocks!" << endl;
+
+  //goodbye message
+  cout << "\nCongratulations Rock Collectors!" << endl;
+
+
   return 0;
 }
-
-// Function implementations (if any)
-
-
-// ------------- DESIGN -------------
-/* 
-Program Name:
-
-Program Description:
-
-Design:
-A. INPUT
-Define the input variables including name data type. 
-
-B. OUTPUT
-Define the output variables including data types. 
-
-C. CALCULATIONS
-Describe calculations used by algorithms in step D.  
-List all formulas. 
-If there are no calculations needed, state there are no calculations.
-
-D. LOGIC and ALGORITHMS
-Design the logic of your program using pseudocode or flowcharts. 
-Use conditionals, loops, functions or array constructs.
-List the steps in transforming inputs into outputs. 
-https://github.com/Glen-Sasek-PCC-Instructor/2025-06-22/blob/main/Pseudocode-Reference.txt
-
-
-SAMPLE RUNS
-Copy from assignment document.
-
-*/
